aa64c01a9dfc52c8311430cd9dcdeeaec3188788
Victor Golovanenko <drygdryg2014@yandex.com>
2021-08-18 12:57:08 +0300

    Active recognition: improved recognition for KYOCERA printers

--- active_recognition.py
+++ active_recognition.py
@@ -2,13 +2,14 @@ import re
 from typing import Optional
 
 import requests
+from lxml import html
 
 from utils import Device
 
 
-def safe_http_get(url, params: dict = None, timeout=3) -> Optional[requests.Response]:
+def safe_http_get(url, params: dict = None, timeout=3, **kwargs) -> Optional[requests.Response]:
     try:
-        r = requests.get(url, params=params, timeout=timeout, verify=False)
+        r = requests.get(url, params=params, timeout=timeout, verify=False, **kwargs)
     except requests.exceptions.RequestException:
         return
     else:
@@ -22,6 +23,12 @@ def recognize_by_http(ip: str, port=80, http_timeout=3) -> Optional[Device]:
     if not r:
         return
 
+    # Recognize device by the Server response header
+    if 'Server' in r.headers:
+        server_header = r.headers['Server']
+        if match := re.search(r'HP (DesignJet \w+) MFP', server_header):
+            return Device('HP', match.group(1), 'MFP', None)
+
     # Recognize device by the response body
     if 'SeWoo Ethernet IP Config' in r.text:
         return Device('SeWoo', 'printer', 'Printer', None)
@@ -31,14 +38,22 @@ def recognize_by_http(ip: str, port=80, http_timeout=3) -> Optional[Device]:
     elif ('Zebra Technologies' in r.text) and (match := re.search(r'ZTC ([\w-]+)', r.text)):
         return Device('Zebra', match.group(1), 'Printer', None)
     elif 'KYOCERA MITA' in r.text:
-        return Device('KYOCERA', 'printer', 'Printer', None)
+        manufacturer = 'KYOCERA'
+        if r := safe_http_get(
+                f'{base_url}/js/jssrc/model/startwlm/Start_Wlm.model.htm',
+                headers={'Referer': f'{base_url}/startwlm/Start_Wlm.htm'}, cookies={'rtl': '0'}):
+            if match := re.search(r"f_getPrinterModel = 'ECOSYS (\w+)';", r.text):
+                return Device(manufacturer, f'ECOSYS {match.group(1)}', 'Printer', None)
+        return Device(manufacturer, 'printer', 'Printer', None)
     elif match := re.search(r'HP(?: Color)? LaserJet MFP (\w+)', r.text):
         return Device('HP', f'LaserJet {match.group(1)}', 'MFP', None)
     elif match := re.search(r'HP(?: Color)? LaserJet Pro MFP (\w+)', r.text):
         return Device('HP', f'LaserJet Pro {match.group(1)}', 'MFP', None)
 
-    # Recognize device by the Server response header
-    if 'Server' in r.headers:
-        server_header = r.headers['Server']
-        if match := re.search(r'HP (DesignJet \w+) MFP', server_header):
-            return Device('HP', match.group(1), 'MFP', None)
+    # Try to detect and follow HTML redirect
+    if ('Content-Type' in r.headers) and (r.headers['Content-Type'] == 'text/html'):
+        url = html.fromstring(r.text).xpath('//meta[@http-equiv="refresh"]/@content')
+        if url:
+            url = url[0][url[0].lower().find('url=') + 4:]
+            r = safe_http_get(f'{base_url}/{url}')
+            # TODO
